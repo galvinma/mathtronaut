a75efd4ab40253128ecbdcaf9d039bbb7a8eeac1
Matthew Galvin <mattgalvin47@gmail.com>
2018-09-13 23:32:45 -0700

    logging for api

--- app.py
+++ app.py
@@ -13,12 +13,16 @@ CORS(app)
 db = TinyDB('scores.json')
 
 def insert_user(username, score):
+    print("In insert_user. Inserting user...")
     db.insert({'username': username, 'score': int(score), 'timestamp': str(datetime.datetime.now())})
     return
 
 def remove_lowest_score():
+    print("In remove_lowest_score. Removing user...")
     Q = Query()
     low = int(min([x['score'] for x in db.all()]))
+    print("Low score is... "+str(low))
+    print("Q.score is ..."+str(Q.score))
     db.remove(Q.score <= low)
     return
 
@@ -31,21 +35,27 @@ def return_leaderboard():
 def is_top():
     score = int(request.args.get('score'))
     if len(db.all()) >= 0 and len(db.all()) < 25:
+        print("In is_top. len of db is >= 0 and < 25. Returning True")
         return jsonify(True)
     if len(db.all()) >= 25:
+        print("In is_top. len of db is > 25")
         low = int(min([x['score'] for x in db.all()]))
         if score > low:
             return jsonify(True)
+            print("Returning True")
         else:
             return jsonify(False)
+            print("Returning False")
 
 @app.route("/api/v1/insertuser", methods=['GET'])
 def insert():
     # Get result
+    print("In insert(). Calling insert_user()...")
     username = request.args.get('username')
     score = request.args.get('score')
     insert_user(username, score)
     if len(db.all()) > 25:
+        print("In insert(). Calling remove_lowest_score() because len of db is > 25")
         remove_lowest_score()
     return jsonify(None)
 

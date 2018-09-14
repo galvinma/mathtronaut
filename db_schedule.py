2fdf710c77dda6edb31f554aa445d999fc8900b0
Matthew Galvin <mattgalvin47@gmail.com>
2018-09-14 14:51:26 -0700

    update db purge script

--- db_schedule.py
+++ db_schedule.py
@@ -1,16 +1,25 @@
 import datetime
 import time
 import schedule
-from tinydb import TinyDB, Query
+from sqlalchemy import exists, create_engine, exc
+from sqlalchemy.orm import sessionmaker, scoped_session, query
+from sqlalchemy.ext.declarative import declarative_base
 
-db = TinyDB('scores.json')
+from model import DeclarativeBase, HighScores
+
+original_engine = create_engine('postgresql://mathtronaut@localhost/mathtronaut')
+Session = sessionmaker(bind=original_engine)
+metadata = DeclarativeBase.metadata
+metadata.create_all(original_engine)
+session = Session()
 
 def purgeOldScores():
-    Q = Query()
-    for x in db.all():
-        strip = datetime.datetime.strptime(x['timestamp'], "%Y-%m-%d %H:%M:%S.%f")
+    q = session.query(HighScores).order_by(HighScores.score)
+    for x in q.all():
+        strip = datetime.datetime.strptime(x.timestamp, "%Y-%m-%d %H:%M:%S.%f")
         if datetime.datetime.now() - strip > datetime.timedelta(seconds=86400):
-            db.remove(Q.username == x['username'])
+            session.delete(x)
+            session.commit()
 
 schedule.every(10).minutes.do(purgeOldScores)
 

2d2ce1de292cbed974954e575b1f4114557e51bd
Matthew Galvin <mattgalvin47@gmail.com>
2018-09-17 11:38:42 -0700

    update about page styles

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -29,17 +29,23 @@ const About = (props) => (
   <div>
     <Grid>
       <Row className="show-grid">
-          <Col sm={6} md={6}>
-            <Jumbotron className="about">
-            <div id="about_info">
-              <div>Mathtronaut is an open source space themed website designed to make learning mathmatics fun. If you like mathtronaut, please consider donating:</div>
-              <ul>
-                <li id="ether">ether: 0x2Fd28424005a969Ac799953aF1e10E8A536DF8EE</li>
-                <li id="btc">btc: 1PUknv1RUpN1zmVQhFDnLrtmn8hSdU1VUB</li>
-              </ul>
-              <div>If you have any questions or comments you can reach the developer at mathtronaut.game@gmail.com</div>
-              </div>
-            </Jumbotron>
+          <Col smOffset={2} sm={8} mdOffset={2} md={8}>
+            <div id="about_container">
+              <Jumbotron className="about">
+                <div id="about_info">Mathtronaut is an<a className="aboutlink" target="_blank" href="https://github.com/galvinma/mathtronaut">open source</a> space themed website designed to make learning mathmatics fun. If you like mathtronaut, please consider donating.</div>
+                <br />
+                <div>
+                <ul id="donation_list">
+                  <li id="coffee"><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/Wd0ji9Y6O"><img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee" /><span id="coffee_span">Buy me a coffee</span></a></li>
+                  <br />
+                  <li id="ether">eth: 0x2Fd28424005a969Ac799953aF1e10E8A536DF8EE</li>
+                  <li id="btc">btc: 1PUknv1RUpN1zmVQhFDnLrtmn8hSdU1VUB</li>
+                </ul>
+                </div>
+                <br />
+                <div>Questions or comments? You can reach the developer at mathtronaut.game@gmail.com</div>
+              </Jumbotron>
+            </div>
           </Col>
       </Row>
     </Grid>

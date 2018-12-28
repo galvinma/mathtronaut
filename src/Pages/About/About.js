b770a4dfa3348c998bea8dcf0f9f6684e05bd01e
Matthew Galvin <mattgalvin47@gmail.com>
2018-12-28 13:57:54 -0800

    update about content

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -25,6 +25,15 @@ const methods = {
   componentDidMount
 };
 
+// <div>
+// <ul id="donation_list">
+//   <li id="coffee"><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/Wd0ji9Y6O"><img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee" /><span id="coffee_span">Buy me a coffee</span></a></li>
+//   <br />
+//   <li id="ether">eth: 0x2Fd28424005a969Ac799953aF1e10E8A536DF8EE</li>
+//   <li id="btc">btc: 1PUknv1RUpN1zmVQhFDnLrtmn8hSdU1VUB</li>
+// </ul>
+// </div>
+
 const About = (props) => (
   <div>
     <Grid>
@@ -32,16 +41,7 @@ const About = (props) => (
           <Col smOffset={2} sm={8} mdOffset={2} md={8}>
             <div id="about_container">
               <Jumbotron className="about">
-                <div id="about_info">Mathtronaut is an<a className="aboutlink" target="_blank" href="https://github.com/galvinma/mathtronaut">open source</a> space themed website designed to make learning mathmatics fun. If you like mathtronaut, please consider donating.</div>
-                <br />
-                <div>
-                <ul id="donation_list">
-                  <li id="coffee"><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/Wd0ji9Y6O"><img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee" /><span id="coffee_span">Buy me a coffee</span></a></li>
-                  <br />
-                  <li id="ether">eth: 0x2Fd28424005a969Ac799953aF1e10E8A536DF8EE</li>
-                  <li id="btc">btc: 1PUknv1RUpN1zmVQhFDnLrtmn8hSdU1VUB</li>
-                </ul>
-                </div>
+                <div id="about_info">Mathtronaut is an<a className="aboutlink" target="_blank" href="https://github.com/galvinma/mathtronaut">open source</a> space themed website designed to make learning mathmatics fun.</div>
                 <br />
                 <div>Questions or comments? You can reach the developer at mathtronaut.game@gmail.com</div>
               </Jumbotron>
